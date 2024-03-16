2fc0997013aca0d76caa063368e76459a7629129
14-sahil <karamkarsahil14@gmail.com>
2024-03-17 02:05:26 +0530

    first commit

---
+++ src/electricity/billing/system/Conn.java
@@ -0,0 +1,15 @@
+
+package electricity.billing.system;
+import java.sql.*;
+public class Conn {
+     Connection c;
+    Statement s;
+    Conn() {
+        try {
+            c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "it@123");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
